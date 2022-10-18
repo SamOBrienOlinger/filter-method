61b132da3d5b5bfe42cd1c7e9d150581ca64ec0d
SamOBrienOlinger <samtobo@yahoo.co.uk>
2022-10-18 16:48:06 +0000

    save examples of filter method

--- filter.js
+++ filter.js
@@ -28,12 +28,13 @@ console.log(getEm);
 getPaul = people.filter(person => person.name === 'Paul'); 
 console.log(getPaul);
 
+//my wrong attempt
 /*getAnother = people.filter(p => p.name === ([0], [2])); 
-console.log(getAnother);*/
+console.log(getAnother);*/ 
 
 
 // Complex Filtering
-const students = [
+/*const students = [
   {
     id: 1,
     name: 'Mark',
@@ -64,7 +65,7 @@ const students = [
       { name: 'css', yrsExperience: 5 },
     ]
   },
-];
+];*/
 
 
 
@@ -81,10 +82,49 @@ const candidate = students.filter(hasSkilzos);
 
 console.log(candidate);*/
 
-const experience = skill => skill.yrsExperience >= 5
+
+
+/*const experience = skill => skill.yrsExperience >= 5
 
 const hasSkilzos =  s => s.skills.filter(experience).length > 0;
   
 const candidate = students.filter(hasSkilzos);
 
-console.log(candidate);
+console.log(candidate);*/
+
+let students = [
+  {
+      name: 'John',
+      subjects: ['maths', 'english', 'cad'],
+      teacher: {maths: 'Harry', english: 'Joan', cad: 'Paul'},
+      results: {maths: 90, english: 75, cad: 87},
+  },
+  {
+      name: 'Emily',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 93, english: 80, art: 95},
+  },
+  {
+      name: 'Adam',
+      subjects: ['science', 'maths', 'art'],
+      teacher: {science: 'Iris', maths: 'Harry', art: 'Simon'},
+      results: {science: 84, maths: 97, art: 95},
+  },
+  {
+      name: 'Fran',
+      subjects: ['science', 'english', 'art'],
+      teacher: {science: 'Iris', english: 'Joan', art: 'Simon'},
+      results: {science: 67, english: 87, art: 95},
+  }
+];
+
+const topMaths = students.filter(student => {
+
+  let goodGrades = student.results.filter(result => result.maths <= 90);
+  
+  return goodGrades.lenght > 0; 
+
+});
+
+console.log(topMaths);
